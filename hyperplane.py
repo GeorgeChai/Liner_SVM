4c468224107c3541c7ac7544c5016d482fb7ad03
GeorgeChai <42084861+GeorgeChai@users.noreply.github.com>
2019-11-05 15:02:44 +0800

    updated_hyperplane.py

--- hyperplane.py
+++ hyperplane.py
@@ -6,5 +6,9 @@ def hyperplane(w,b,start,end):
 
     x_points = np.linspace(start, end, 2)
     y_ = -(w[0] * x_points + b) / w[1]
+    y_plus = -(w[0] * x_points + b + 1) / w[1]
+    y_sub = -(w[0] * x_points + b - 1) / w[1]
     plt.plot(x_points, y_)
+    plt.plot(x_points, y_plus, linewidth=2, linestyle="--")
+    plt.plot(x_points, y_sub, linewidth=2, linestyle="--")
     plt.show()
